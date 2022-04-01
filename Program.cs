01ae5b9e9d331272933f24090c676c1d26cfd0b3
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-01 22:28:55 +0530

    Permutation using recursion

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace WelcomeToAlogorithms
+namespace AlgorithmPrograms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome TO The Algorithms!");
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("1.Permutation Using Recursion");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Enter String to find Permutation ");
+                    string str = Console.ReadLine();
+                    int n = str.Length;
+                    Permutations.Permutate(str, 0, n - 1);
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the valid option!");
+                    break;
+            }
+            Console.WriteLine("\nDo you want to continue");
+            Console.ReadLine();
         }
     }
 }
